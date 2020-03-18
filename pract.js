2f9d212a6475703f6d0d210653b564c2a8a2b352
Ekaterina <katya.cuzina2011@yandex.ru>
2020-03-18 11:35:44 +0300

    second lesson on udemy

---
+++ pract.js
@@ -0,0 +1,35 @@
+/*let num = 45;
+if(num < 49){
+    console.log("Неверно!")
+} else if( num > 100) {
+    console.log("Много!")
+} else {
+    console.log("Верно!")
+}
+'use strict';
+
+    let value = prompt('Какое "официальное" название JavaScript?', '');
+
+    if (value == 'ECMAScript') {
+      console.log('Верно!');
+    } else {
+      console.log('Не знаете? ECMAScript!');
+    }*/
+    let num = 50;
+    //while(num<55) {
+     //   console.log(num);
+      //  num++;
+   // }
+   /*do{
+       console.log(num);
+       num++;
+   }
+   while(num<55)*/
+
+   for(let i = 1; i<8; i++){
+       if(i == 6){
+        continue;
+       }
+       console.log(i);
+   }
+
